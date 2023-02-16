fixing Softmax and MeanAbsoluteError

deeplib/layers.py
@@ -49,7 +49,7 @@ class Dense(Layer):
         #print(chained_grad.shape, weights.shape)
         gradients = np.matmul(chained_grad, weights)
         
-        return gradients # vector (1, input_size)
+        return gradients # vector (batch_size, input_size)
     
 # class Conv2D(Layer):
     
@@ -107,26 +107,43 @@ class Softmax(Layer):
         self.trainable = False
     
     def __call__(self, x:np.ndarray) -> np.ndarray:
-        super().__call__(x)        
-        exp = np.exp(x)
-        self.output = exp / np.sum(exp)
+        super().__call__(x)
+        softmax = []     
+        for sample in x:
+            exp = np.exp(sample)
+            out = exp / np.sum(exp)
+            softmax.append(out)
     
+        self.output = np.array(softmax)
+
         return self.output
     
     def backward(self, chained_grad:np.ndarray) -> np.ndarray:
-        """Unvectorized computation of the gradient of softmax.
-        z: (T, 1) column array of input values.
-        Returns D (T, T) the Jacobian matrix of softmax(z) at the given z. D[i, j]
-        is DjSi - the partial derivative of Si w.r.t. input j.
+        """                        dy1   dy1
+                                  ----- ----- ...
+                                   dx1   dx2
+                        
+             d(y1,y2,y3,y4,y5)     dy2
+        J = ------------------- = -----  ...
+             d(x1,x2,x3,x4,x5)     dx1
+                                    
+                                   ...
+             
+                y1      1  0  0  0  0
+                y2      0  1  0  0  0
+        J =     y3  *  (0  0  1  0  0  - [y1 y2 y3 y4 y5])   
+                y4      0  0  0  1  0
+                y5      0  0  0  0  1
+                
+        J = softmax(x) - (I - softmax(x).T)
         """
-        jacobians = []
-        for sample, output in zip(self.input, self.output):
-            N = sample.shape[0]
-            D = np.zeros((N, N))
-            for i in range(N):
-                for j in range(N):
-                    D[i, j] = output[i] * (np.float32(i == j) - output[j])
-            jacobians.append(D)
-        return np.array(jacobians)
+        gradients = []
+        for sample, output, sample_grad in zip(self.input, self.output, chained_grad):
+            I = np.eye(sample.shape[0])
+            J = output  * (I - output.transpose())
+            grad = np.matmul(sample_grad, J)
+            gradients.append(grad)
+          
+        return np.array(gradients)
     
 ###########################################################

deeplib/losses.py
@@ -29,24 +29,35 @@ class MeanAbsoluteError(Loss):
     
     def __call__(self, y_pred:np.ndarray, y_true:np.ndarray) -> Any:
         super().__call__(y_pred, y_true)
-        return (y_pred - y_true)**2
+        loss = []
+        for pred, true in zip(y_pred, y_true):
+            l = (pred - true)**2 
+            loss.append(l)
+        
+        return np.array(loss)
     
     def backward(self):
-        return 2*(self.y_pred - self.y_true)
+        loss_gradient = []
+        for pred, true in zip(self.y_pred, self.y_true):
+            lg = 2*(pred - true)
+            loss_gradient.append(lg)
+
+        return np.array(loss_gradient)
+    
 
 
-class BCELoss(Loss):
+# class BCELoss(Loss):
     
-    def __init__(self) -> None:
-        pass
+#     def __init__(self) -> None:
+#         pass
     
-    def __call__(self, y_pred:np.ndarray, y_true:np.ndarray) -> float:
-        super().__call__(y_pred, y_true)
-        y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
-        term_0 = (1-y_true) * np.log(1-y_pred + epsilon)
-        term_1 = y_true * np.log(y_pred + epsilon)
+#     def __call__(self, y_pred:np.ndarray, y_true:np.ndarray) -> float:
+#         super().__call__(y_pred, y_true)
+#         y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+#         term_0 = (1-y_true) * np.log(1-y_pred + epsilon)
+#         term_1 = y_true * np.log(y_pred + epsilon)
         
-        return -np.mean(term_0+term_1, axis=0)
+#         return -np.mean(term_0+term_1, axis=0)
     
     
 class CrossEntropyLoss(Loss):
@@ -56,21 +67,14 @@ class CrossEntropyLoss(Loss):
     
     def __call__(self, y_pred:np.ndarray, y_true:np.ndarray) -> np.ndarray:
         super().__call__(y_pred, y_true)
-        if len(y_pred.shape) > 1:
-            total_loss = []
-            for pred, true in zip(y_pred, y_true):
-                loss = self.loss(pred, true)
-                total_loss.append(loss)
-        else:
-            total_loss = [self.loss(y_pred, y_true)]
+        total_loss = []
+        for pred, true in zip(y_pred, y_true):
+            loss = -np.sum(true * np.log(pred + epsilon))
+            total_loss.append(loss)
         
         total_loss = np.array(total_loss)
         
         return total_loss
-         
-    def loss(self, y_pred, y_true) -> float:
-        """ 1D vectors """
-        return -np.sum(y_true * np.log(y_pred + epsilon))
 
     def backward(self):
         """Gradient of the cross-entropy loss function for p and y.
@@ -79,13 +83,23 @@ class CrossEntropyLoss(Loss):
                 one element of y is 1; the rest are 0.
         Returns a (1, T) Jacobian for this function.
         """
-        assert(self.y_pred.shape == self.y_true.shape and self.y_pred.shape[1] == 1)
-        # py is the value of p at the index where y == 1 (one and only one such
-        # index is expected for a one-hot y).
-        py = self.y_pred[self.y_true == 1]
-        assert(py.size == 1)
-        # D is zeros everywhere except at the index where y == 1. The final D has
-        # to be a row-vector.
-        D = np.zeros_like(self.y_pred)
-        D[self.y_pred == 1] = -1/py.flat[0]
-        return D.flatten()
+        #print(self.y_pred, self.y_pred.shape, self.y_true, self.y_true.shape)
+        loss_gradient = []
+        for pred, true in zip(self.y_pred, self.y_true):
+            pred = np.expand_dims(pred, axis=-1)
+            true = np.expand_dims(true, axis=-1)
+            assert(pred.shape == true.shape and pred.shape[1] == 1)
+            # py is the value of p at the index where y == 1 (one and only one such
+            # index is expected for a one-hot y).
+            py = pred[true == 1]
+            #print(py)
+            assert(py.size == 1)
+            # D is zeros everywhere except at the index where y == 1. The final D has
+            # to be a row-vector.
+            D = np.zeros_like(pred)
+            D[pred == 1] = -1/py.flat[0]
+            loss_gradient.append(D.flatten())
+        
+        loss_gradient = np.array(loss_gradient)
+        print(loss_gradient, loss_gradient.shape)
+        return loss_gradient

deeplib/models.py
@@ -25,7 +25,6 @@ class Model(ABC):
         for layer in self.layers:
             x = layer(x)
         return x
-        
 
 class Trainer:
     
@@ -52,14 +51,22 @@ class Trainer:
                 outputs = np.squeeze(outputs)
                 # ======================== Step Metrics ========================
                 train_loss = criterion(outputs, labels)
-                if len(train_loss) > 1:
-                    # Multilabel classification
-                    train_accuracy = 0
-                else:
-                    # Binary classification
-                    rounded_outputs = np.round(outputs)
-                    train_accuracy = (labels == rounded_outputs).sum() / len(labels)
                 train_loss = np.mean(train_loss)
+                
+                # -- Metrics
+                if len(labels.shape) > 1:
+                    # Multi-class
+                    y_true = np.argmax(labels, axis=1)
+                    y_pred = np.argmax(outputs, axis=1)
+                else:
+                    # Binary-class
+                    y_true = labels
+                    y_pred = np.where(outputs > 0.5, 1, 0)
+                    
+                y_true = y_true.astype(np.uint0)
+                y_pred = y_pred.astype(np.uint0)
+                
+                train_accuracy = ((y_true == y_pred).sum() / len(y_true))
                 # ========= Calculate gradients, Backpropagate error and Update Weights =========
                 optimizer.step(criterion.backward())
                 # ================= Update Train Metrics Info ==================