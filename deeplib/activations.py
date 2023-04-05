Fixing Tensor class gradient calculation

deeplib/activations.py
@@ -1,11 +1,12 @@
 
-from .engine import Tensor
-
+from deeplib.engine import Tensor
 import numpy as np
 
-class Relu:
+
+class ReLU:
     
     def __call__(self, x:Tensor) -> Tensor:
+        assert isinstance(x, Tensor), "Input must be a Tensor"
         out = Tensor(np.maximum(0, x.data), (x,), 'ReLU')
 
         def _backward():
@@ -19,31 +20,39 @@ class Relu:
 class Sigmoid:
     
     def __call__(self, x:Tensor) -> Tensor:
-        super().__call__(x)
-        return (1/(1 + np.exp(-x)))
+        assert isinstance(x, Tensor), "Input must be a Tensor"
+        out = Tensor(1/(1 + np.e**-x))
     
-    def backward(self, grad):
-        """ grad is the """
-        f = 1/(1 + np.exp(-self.input))
-        df = f * (1 - f)
+        def _backward():
+            f = 1/(1 + np.exp(-x.data))
+            x_grad = f * (1 - f)
+            
+            x.grad = (x_grad) * out.grad
+            
+        out._backward = _backward
         
-        return df
+        return out
     
-class Softmax:
+if __name__ == "__main__":
+    a = Tensor([-1,-2,4,5,1,7])
+    b = Sigmoid()(a)
+    b.backward()
+    print(a)
     
-    def __init__(self) -> None:
-        super().__init__()
-        self.trainable = False
+# class Softmax:
     
-    def __call__(self, x:np.ndarray) -> np.ndarray:
-        super().__call__(x)
-        softmax = []     
-        for sample in x:
-            exp = np.exp(sample)
-            out = exp / np.sum(exp)
-            softmax.append(out)
+#     def __init__(self) -> None:
+#         super().__init__()
+#         self.trainable = False
     
-        self.output = np.array(softmax)
+#     def __call__(self, x:np.ndarray) -> np.ndarray:
+#         super().__call__(x)
+#         softmax = []     
+#         for sample in x:
+#             exp = np.exp(sample)
+#             out = exp / np.sum(exp)
+#             softmax.append(out)
+    
+#         self.output = np.array(softmax)
 
-        return self.output
-    
+#         return self.output

deeplib/engine.py
@@ -1,39 +1,42 @@
-from abc import abstractclassmethod, ABC
 import numpy as np
 
 
-class Module(ABC):
+class Module:
 
     def zero_grad(self):
         for p in self.parameters():
             p.grad = 0
 
-    @abstractclassmethod
     def parameters(self):
         return []
 
 
 class Tensor:
     
-    def __init__(self, data, _children=(), _operation='') -> None:
+    def __init__(self, data, _children=(), _operation='None', requires_grad=True) -> None:
         if not isinstance(data, np.ndarray):
             data = np.array(data)
         assert isinstance(data, np.ndarray), "data must be a list or numpy array"
         self.data = data
-        self.grad = np.zeros(data.shape)
+        self.requires_grad = requires_grad
+        self.grad = None
         # internal variables used for autograd graph construction
-        self._backward = lambda: None
+        self._backward = None
         self._children = _children
         self._operation = _operation # the operation that produced this node, for graphviz / debugging
+        
+        if requires_grad:
+            self.grad = np.zeros(data.shape)
+            self._backward = lambda: None
     
     
     def __add__(self, summand):
         summand = summand if isinstance(summand, Tensor) else Tensor(summand)
-        out = Tensor(self.data + summand.data, (self, summand), '+')
+        out = Tensor(self.data + summand.data, (self, summand), '+', requires_grad=self.requires_grad)
         
         def _backward():
-            self.grad += out.grad
-            summand.grad += out.grad
+            self.grad += out.grad.sum() if len(self.data.shape) == 0 else out.grad
+            summand.grad += out.grad.sum() if len(summand.data.shape) == 0 else out.grad
         
         out._backward = _backward 
         
@@ -41,11 +44,14 @@ class Tensor:
         
     def __mul__(self, factor):
         factor = factor if isinstance(factor, Tensor) else Tensor(factor)
-        out = Tensor(self.data * factor.data, (self, factor), '*')
+        out = Tensor(self.data * factor.data, (self, factor), '*', requires_grad=self.requires_grad)
         
         def _backward():
-            self.grad += factor.data * out.grad
-            factor.grad += self.data * out.grad
+            self_grad = factor.data * out.grad
+            self.grad += self_grad.sum() if len(self.data.shape) == 0 else self_grad
+            
+            factor_grad = self.data * out.grad
+            factor.grad += factor_grad.sum() if len(factor.data.shape) == 0 else factor_grad
             
         out._backward = _backward 
         
@@ -53,10 +59,11 @@ class Tensor:
     
     def __pow__(self, power):
         assert isinstance(power, (int, float)), f"Power of type '{type(power)}' not supported"
-        out = Tensor(self.data**power, (self,), f'**{power}')
+        out = Tensor(self.data**power, (self,), f'**{power}', requires_grad=self.requires_grad)
         
         def _backward():
-            self.grad += (power * self.data**(power-1)) * out.grad
+            self_grad = (power * self.data**(power-1)) * out.grad
+            self.grad += self_grad.sum() if len(self.data.shape) == 0 else self_grad
                        
         out._backward = _backward
         
@@ -64,10 +71,11 @@ class Tensor:
     
     def __rpow__(self, power):
         assert isinstance(power, (int, float)), f"Power of type '{type(power)}' not supported"
-        out = Tensor(np.power(power, self.data), (self,), f'{power}**')
+        out = Tensor(np.power(power, self.data), (self,), f'{power}**', requires_grad=self.requires_grad)
         
         def _backward():
-            self.grad += (power**self.data * np.log(power)) * out.grad
+            self_grad += (power**self.data * np.log(power)) * out.grad
+            self.grad += self_grad.sum() if len(self.data.shape) == 0 else self_grad
                        
         out._backward = _backward
         
@@ -75,17 +83,32 @@ class Tensor:
     
     def __matmul__(self, tensor):
         tensor = tensor if isinstance(tensor, Tensor) else Tensor(tensor)
-        out = Tensor(np.matmul(self.data, tensor.data), (self, tensor), 'x')
+        out = Tensor(np.matmul(self.data, tensor.data), (self, tensor), 'x', requires_grad=self.requires_grad)
         
         def _backward():
-            self.grad += tensor.data * out.grad
-            tensor.grad += self.data * out.grad
+            self_grad = tensor.data * out.grad
+            self.grad += self_grad.sum() if len(self.data.shape) == 0 else self_grad
+            
+            tensor_grad = self.data * out.grad
+            tensor.grad += tensor_grad.sum() if len(tensor.data.shape) == 0 else tensor_grad
                        
         out._backward = _backward
         
         return out
     
-    def backward(self):
+    def backward(self, grad=None):
+        if not self.requires_grad:
+            raise RuntimeError("Trying to call backward on Tensor with requires_grad=False")
+        
+        if self._backward is None:
+            self._backward = lambda: None
+
+        if grad is None:
+            if self.data.size > 1:
+                raise RuntimeError("grad must be specified for non-scalar tensors")
+            else:
+                grad = 1
+        
         # topological order all of the children in the graph
         ordered_nodes = []
         visited_nodes = set()
@@ -98,7 +121,7 @@ class Tensor:
         visit_node(self)
 
         # go one variable at a time and apply the chain rule to get its gradient
-        self.grad = 1
+        self.grad = grad
         for v in reversed(ordered_nodes):
             v._backward()
     
@@ -132,44 +155,117 @@ class Tensor:
         return other * self**-1
         
     def __repr__(self) -> str:
-        return f"Tensor(value={self.data}, grad={self.grad})"
+        return f"Tensor(value={self.data}, shape={self.shape}, op={self._operation})"
+    
+   
+    @staticmethod
+    def concat(tensors, dim=0):
+        # Check that all tensors have the same shape along the specified dim
+        dim_sizes = [tensor.shape[dim] for tensor in tensors]
+        assert all(size == dim_sizes[0] for size in dim_sizes), f"Shapes along dim {dim} don't match: {[tensor.shape for tensor in tensors]}"
+
+        # Create a list of slice objects to select the correct section of each tensor
+        slices = []
+        start = 0
+        for tensor in tensors:
+            slices.append(slice(start, start + tensor.shape[dim]))
+            start += tensor.shape[dim]
+
+        # Concatenate the sections along the specified dim
+        if dim == 0:
+            new_data = np.concatenate([tensor.data for tensor in tensors], axis=dim)
+        else:
+            new_data = np.concatenate([tensor.data.take(s, axis=dim) for tensor, s in zip(tensors, slices)], axis=dim)
+
+        out = Tensor(new_data, tensors, _operation='concatenate', requires_grad=True)
+
+        def _backward():
+            # Split the gradient along the concatenated dim and backpropagate to each input tensor
+            grads = np.split(out.grad, len(tensors), axis=dim)
+            for tensor, grad in zip(tensors, grads):
+                if dim == 0:
+                    tensor.grad += grad
+                else:
+                    tensor.grad += grad.take(slices[tensors.index(tensor)], axis=dim)
+
+        out._backward = _backward
         
+        return out
     
+    def view(self, shape:tuple):
+        out = Tensor(self.data.reshape(shape), (self,), 'view', requires_grad=self.requires_grad)
+        
+        def _backward():
+            self.grad += out.grad.reshape(self.shape)
+            
+        out._backward = _backward
+        
+        return out
+    
+    def unsqueeze(self, dim):
+        data = np.expand_dims(self.data, dim)
+        out = Tensor(data, (self,), 'unsqueeze', requires_grad=self.requires_grad)
+        
+        def _backward():
+            self.grad += np.squeeze(out.grad, dim)
+            
+        out._backward = _backward
+        
+        return out
+    
+    def sum(self, dim=None):
+        out = Tensor(self.data.sum(axis=dim), (self,), 'sum', requires_grad=self.requires_grad)
+        
+        def _backward():
+            self.grad += np.ones(self.shape) * out.grad
+            
+        out._backward = _backward
+        
+        return out
+
+# Check with pytorch that gradients are correct when applying different tensor operations
 if __name__ == "__main__":
-    l1 = [-4.0, 5.0]
-    l2 = [2.0, 3.0]
+    l1 = [-4.0, 0, 5.0]
+    l2 = [2.0, 2,  3.0]
     
-    a = Tensor(l1)
-    b = Tensor(l2)
-    c = 2 ** a 
-    c.backward()
+    a = Tensor(l1, requires_grad=True).unsqueeze(0)
+    b = Tensor(l2, requires_grad=True).unsqueeze(0)
+    c = Tensor(4.0, requires_grad=True)
+    out = Tensor.concat((a*c, b), dim=0)
+    out = out.view((3,2))
+    out.sum().backward()
     
     print("Tensor 1: ", a)
     print("Tensor 2: ", b)
-    print("Tensor resultado: ", c)
+    print("Tensor 3: ", c)
+    print("Tensor resultado: ", out)
     print("Gradiente de tensor 1: ", a.grad)
     print("Gradiente de tensor 2: ", b.grad)
+    print("Gradiente de tensor 3: ", c.grad)
     
     import torch
-    from torch import nn
     # Creamos el tensor con valores diferentes
-    tensor1 = torch.tensor(l1, requires_grad=True)
-    tensor2 = torch.tensor(l2, requires_grad=True)
+    a = torch.tensor(l1, requires_grad=True).unsqueeze(0)
+    a.retain_grad()
+    b = torch.tensor(l2, requires_grad=True).unsqueeze(0)
+    b.retain_grad()
+    c = torch.tensor(4.0, requires_grad=True)
+    c.retain_grad()
 
     # Realizamos la multiplicación entre los dos tensores
-    tensor_resultado = 2 ** tensor1 
+    out = torch.concat((a*c, b), dim=0)
+    out = out.view((3,2))
 
     # Calculamos el gradiente del tensor resultado
-    if len(tensor_resultado.shape) >= 1:
-        tensor_resultado.backward(torch.ones(tensor_resultado.shape))
-    else:
-        tensor_resultado.backward()
+    out.sum().backward()
 
     # Mostramos los resultados
-    print("Tensor 1: ", tensor1)
-    print("Tensor 2: ", tensor2)
-    print("Tensor resultado: ", tensor_resultado)
-    print("Gradiente de tensor 1: ", tensor1.grad)
-    print("Gradiente de tensor 2: ", tensor2.grad)
+    print("\nTensor 1: ", a)
+    print("Tensor 2: ", b)
+    print("Tensor 3:", c)
+    print("Tensor resultado: ", out)
+    print("Gradiente de tensor 1: ", a.grad)
+    print("Gradiente de tensor 2: ", b.grad)
+    print("Gradiente de tensor 3: ", c.grad)
     
     

deeplib/layers.py
@@ -1,6 +1,6 @@
 
-from .engine import Tensor, Module
-from .neurons import Neuron
+from deeplib.engine import Tensor, Module
+from deeplib.neurons import Neuron
 
 import numpy as np
 
@@ -13,16 +13,36 @@ class Linear(Module):
         self.output_size = output_size
         self.neurons = []
         for _ in range(self.output_size):
-            n = Neuron(input_size, output_size, weight_init_method=weight_init_method)
+            n = Neuron(input_size, weight_init_method=weight_init_method)
             self.neurons.append(n)
         
-    def __call__(self, x:Tensor) -> Tensor:
-        assert x.shape[1] == self.input_size, f"Expected input size '{self.input_size}' but received '{x.shape[1]}'"
+    def __call__(self, x:Tensor) -> list:
+        assert x.shape[0] == self.input_size, f"Expected input size '{self.input_size}' but received '{x.shape[0]}'"
         
-        output = np.array([ neuron(x) for neuron in self.neurons ]).transpose()
-        assert len(output[0]) == self.output_size, f"CODE ERROR, ouput does not have the correct size {len(output[0])} != {self.output_size}"
+        activations = [ neuron(x) for neuron in self.neurons ]
+        print(activations)
+        out = Tensor.concat(activations, axis=0)
+        #assert len(output.shape[1]) == self.output_size, f"CODE ERROR, ouput does not have the correct size {len(output[0])} != {self.output_size}"
         
-        return output
+        return out
+    
+    def parameters(self):
+        return [p for n in self.neurons for p in n.parameters()]
+        
+    
+if __name__ == "__main__":
+    list_ = [0.2, 1,4,2, -1, 4]
+    inp = Tensor(list_)
+    linear = Linear(6, 1)
+    out = linear(inp)
+    out.backward()
+    print(inp)
+    
+    # print(inp.shape)
+    # lin = Linear(inp.shape[1], 1)
+    # out = lin(inp)
+    # out.backward()
+    # print(inp, lin.parameters())
     
 # class Conv2D(Layer):
     

deeplib/neurons.py
@@ -9,19 +9,22 @@ weight_initializers = ['glorot', 'glorot_norm', 'he']
 
 class Neuron(Module):
     
-    def __init__(self, inputs:int, outputs:int, weight_init_method='he') -> None:
+    def __init__(self, inputs:int, weight_init_method='he') -> None:
         self.inputs = inputs
-        self.outputs = outputs
         # Randomly initialize weights and bias
-        self.weights = Tensor(init_weights(inputs, outputs, weight_init_method))
+        self.weights = Tensor(init_weights(inputs, 1, weight_init_method))
         assert len(self.weights) == inputs, f"{len(self.weights)} {inputs}"
         self.bias = Tensor(0)
     
-    def __call__(self, x:Tensor) -> float:
-        assert x.shape[1] == len(self.weights), f"Expected input size '{len(self.weights)}' but received '{x.shape[1]}'"
+    def __call__(self, x:Tensor) -> Tensor:
+        assert x.shape[0] == len(self.weights), f"Expected input size '{len(self.weights)}' but received '{x.shape[0]}'"
         self.x = x
         self.activation = (x @ self.weights) + self.bias
-        return self.activation
+        
+        return self.activation.unsqueeze(0)
+    
+    def parameters(self):
+        return [self.weights, self.bias]
 
 
 def init_weights(inputs, outputs, method) -> np.ndarray: