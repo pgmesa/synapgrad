adding new activations and losses. Also, some bugs have been fixed. There is still a bug in Softmax functions when computing the gradient that will be addressed in the next commit

synapgrad/engine.py
@@ -38,15 +38,15 @@ def manual_seed(seed:int):
     random.seed(seed)
 
 
-def tensor(data, requires_grad=False) -> 'Tensor':
-    return Tensor(data, requires_grad=requires_grad)
+def tensor(data, requires_grad=False, dtype=np.float32) -> 'Tensor':
+    return Tensor(data, requires_grad=requires_grad, dtype=dtype)
 
 
 class Tensor:
     
-    def __init__(self, data, _children=(), _operation=None, requires_grad=False) -> None:
+    def __init__(self, data, _children=(), _operation=None, requires_grad=False, dtype=np.float32) -> None:
         if not isinstance(data, np.ndarray):
-            data = np.array(data, dtype=np.float64)
+            data = np.array(data, dtype=dtype)
         assert isinstance(data, np.ndarray), "data must be a list or numpy array"
         
         self.data = data
@@ -93,7 +93,7 @@ class Tensor:
         if tensor.grad.matches_shape(grad) or sum_axis is None: 
             if sum_axis is None and not tensor.grad.matches_shape(grad):
                 not_compatible_dims = get_incompatible_dims(tensor_shape, grad_shape)
-                tensor += grad.sum(axis=not_compatible_dims)
+                tensor += grad.sum(axis=not_compatible_dims, keepdims=True)
             else:
                 tensor._grad += grad
         else:
@@ -262,6 +262,21 @@ class Tensor:
         return out
     
     
+    @staticmethod
+    def add(t1:'Tensor', t2:'Tensor') -> 'Tensor':
+        return t1 + t2
+    
+    
+    @staticmethod
+    def mul(t1:'Tensor', t2:'Tensor') -> 'Tensor':
+        return t1 * t2
+    
+    
+    @staticmethod
+    def matmul(t1:'Tensor', t2:'Tensor') -> 'Tensor':
+        return t1 @ t2
+    
+    
     def view(self, shape:tuple) -> 'Tensor':
         out = Tensor(self.data.reshape(shape), (self,), '<View>', requires_grad=self.requires_grad)
         
@@ -305,12 +320,12 @@ class Tensor:
         return out
     
     
-    def sum(self, dim:int=None) -> 'Tensor':
-        out = Tensor(self.data.sum(axis=dim), (self,), '<Sum>', requires_grad=self.requires_grad)
+    def sum(self, dim:int=None, keepdims=False) -> 'Tensor':
+        out = Tensor(self.data.sum(axis=dim, keepdims=keepdims), (self,), '<Sum>', requires_grad=self.requires_grad)
         
         def _backward():
             if self.requires_grad:
-                self._grad += np.ones(self.shape) * out._grad
+                self._grad += out._grad
             
         out._backward = _backward
         
@@ -445,6 +460,10 @@ class Tensor:
         return self.data.shape
     
     @property
+    def dtype(self) -> np.dtype:
+        return self.data.dtype
+    
+    @property
     def size(self) -> int:
         return self.data.size
     
@@ -519,15 +538,28 @@ class Tensor:
 
     def __rtruediv__(self, other) -> 'Tensor': # other / self
         return other * self**-1
+    
+    @staticmethod
+    def pretty_numpy(array:np.ndarray, decimals=4) -> str:
+        rounded_data = array.copy().round(decimals=decimals)
+        #rounded_data += 0. # Remove -0.0 values (just 0.0)
+        str_to_rm = "array("
+        data_str = repr(rounded_data).replace(str_to_rm, "")
+        crop_index = (len(data_str)) - data_str[::-1].find("]")
+        cropped = data_str[:crop_index]
+        
+        braces_padd = (len(array.shape)-1)
+        no_padding = cropped.replace("\n" + " "*(braces_padd+len(str_to_rm)), "\n" + " "*braces_padd)
+        return no_padding
         
     def __repr__(self) -> str:
+        pretty_data_str = self.pretty_numpy(self.data)
         beggining = "Tensor("
         data_str = ""
-        for i, line in enumerate(str(self.data).splitlines(keepends=True)):
+        for i, line in enumerate(pretty_data_str.splitlines(keepends=True)):
             if i != 0:
                 line = " "*len(beggining) + line  
-            data_str += line 
-             
+            data_str += line
         string = f"{beggining}{data_str}, shape={self.shape}"
         
         if self.requires_grad:

synapgrad/nn/activations.py
@@ -2,11 +2,30 @@ import numpy as np
 from .. import Tensor, nn
 
 
+# ---------------------------- Functions ----------------------------
+# -------------------------------------------------------------------
+def relu_fn(data:np.ndarray) -> np.ndarray:
+    return np.maximum(0, data)
+
+
+def sigmoid_fn(data:np.ndarray) -> np.ndarray:
+    return 1/(1 + np.exp(-data))
+
+
+def softmax_fn(data:np.ndarray, dim:int) -> np.ndarray:
+    # Shift to make it numerically stable (with large values 'inf' appears)
+    shiftx = data - data.max(axis=dim, keepdims=True) 
+    exps = np.exp(shiftx)
+    exp_sums = exps.sum(axis=dim, keepdims=True)
+    return exps / exp_sums
+
+# ----------------------------- Modules -----------------------------
+# -------------------------------------------------------------------
 class ReLU(nn.Module):
     
     def forward(self, x:Tensor) -> Tensor:
-        assert isinstance(x, Tensor), "Input must be a Tensor"
-        relu = np.maximum(0, x.data)
+        assert isinstance(x, Tensor), "Input must be a Tensor" 
+        relu = relu_fn(x.data)
         out = Tensor(relu, (x,), '<ReLU>', requires_grad=x.requires_grad)
 
         def _backward():
@@ -24,23 +43,24 @@ class Sigmoid(nn.Module):
         # Returning (1/(1 + np.e**-x)) should be enough, but defining explicit 
         # grad function should be faster
         assert isinstance(x, Tensor), "Input must be a Tensor"
-        sigmoid = 1/(1 + np.exp(-x.data))
+        sigmoid = sigmoid_fn(x.data)
         out = Tensor(sigmoid, (x,), '<Sigmoid>', requires_grad=x.requires_grad)
     
         def _backward():
             if x.requires_grad:
-                f = 1/(1 + np.exp(-x.data))
-                x_grad = f * (1 - f)
-                
+                x_grad = sigmoid * (1 - sigmoid)
                 x._grad += (x_grad) * out._grad
             
         out._backward = _backward
         
         return out
-    
+
     
 class Softmax(nn.Module):
-    """ Reference: https://eli.thegreenplace.net/2016/the-softmax-function-and-its-derivative/ """
+    """ References: 
+            https://eli.thegreenplace.net/2016/the-softmax-function-and-its-derivative/
+            https://aimatters.wordpress.com/2019/06/17/the-softmax-function-derivative/
+    """
     
     def __init__(self, dim) -> None:
         super().__init__()
@@ -48,4 +68,45 @@ class Softmax(nn.Module):
     
     def forward(self, x: Tensor) -> Tensor:
         assert isinstance(x, Tensor), "Input must be a Tensor"
-        return x.exp() / (x.exp().sum(-1)).unsqueeze(-1)
+        softmax = softmax_fn(x.data, self.dim)
+        out = Tensor(softmax, (x,), '<Softmax>', requires_grad=x.requires_grad)
+    
+        def _backward():
+            # Hand made derivation
+            if x.requires_grad:
+                diag = np.stack([np.diag(v) for v in softmax])
+                S_matrix = np.stack([np.tile(v, softmax.shape[self.dim]) for v in softmax]).reshape(diag.shape)
+                S_matrix_T = np.stack([np.transpose(m) for m in S_matrix])
+                grad = diag - (S_matrix * S_matrix_T)
+                x._grad += grad.sum(axis=self.dim) * out._grad
+            
+        out._backward = _backward
+        
+        return out
+
+    
+class LogSoftmax(nn.Module):
+    
+    def __init__(self, dim) -> None:
+        super().__init__()
+        self.dim = dim
+    
+    def forward(self, x: Tensor) -> Tensor:
+        assert isinstance(x, Tensor), "Input must be a Tensor"
+        softmax = softmax_fn(x.data, self.dim)
+        log_softmax = np.log(softmax)
+        out = Tensor(log_softmax, (x,), '<LogSoftmax>', requires_grad=x.requires_grad)
+    
+        def _backward():
+            # Hand made derivation
+            if x.requires_grad:
+                I = np.stack((np.identity(softmax.shape[self.dim]),)*softmax.shape[0])
+                S_matrix = np.stack([np.tile(v, softmax.shape[self.dim]) for v in softmax]).reshape(I.shape)
+                S_matrix_T = np.stack([np.transpose(m) for m in S_matrix])
+                grad = I - S_matrix_T
+                x._grad += grad.sum(axis=self.dim) * out._grad
+            
+        out._backward = _backward
+        
+        return out
+    

synapgrad/nn/layers.py
@@ -10,9 +10,9 @@ class Linear(nn.Module):
         self.input_size = input_size
         self.output_size = output_size
         # Randomly initialize weights and biases
-        weight_values = [ init_weights(input_size, output_size, weight_init_method) for _ in range(output_size) ]
+        weight_values = [ init_weights(input_size, output_size, weight_init_method).astype(np.float32) for _ in range(output_size) ]
         self.weights = Tensor(weight_values, requires_grad=True)
-        self.biases = Tensor(np.zeros(output_size,), requires_grad=True)
+        self.biases = Tensor(np.zeros((output_size,), dtype=np.float32), requires_grad=True)
         
     def forward(self, x:Tensor) -> Tensor:
         assert x.shape[1] == self.input_size, f"Expected input size '{self.input_size}' but received '{x.shape[1]}'"

synapgrad/nn/losses.py
@@ -1,29 +1,67 @@
 from typing import Any
 from abc import ABC, abstractmethod
 from .. import Tensor
+from .activations import softmax_fn, LogSoftmax, relu_fn
 import numpy as np
 
 
 epsilon = 1e-7
 
+# ---------------------------- Functions ----------------------------
+# -------------------------------------------------------------------
+def mse_loss(y_pred: np.ndarray, y_true: np.ndarray) -> np.ndarray:
+    return (y_pred - y_true)**2
+    
+    
+def nll_loss(y_pred: np.ndarray, y_true: np.ndarray) -> np.ndarray:
+    return -y_pred[range(len(y_pred)), y_true].reshape((-1, 1))
 
+
+def bce_loss(y_pred: np.ndarray, y_true: np.ndarray) -> np.ndarray:
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+    term_0 = (1-y_true) * np.log(1-y_pred + epsilon)
+    term_1 = y_true * np.log(y_pred + epsilon)
+    
+    return -(term_0 + term_1)
+
+
+def bce_with_logits_loss(y_pred: np.ndarray, y_true: np.ndarray) -> np.ndarray:
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+    tn = -relu_fn(y_pred)
+    term = (1-y_true) * y_pred + tn + np.log(np.exp(-tn) + np.exp((-y_pred-tn)))
+    
+    return term
+
+
+def cross_entropy_loss(y_pred: np.ndarray, y_true: np.ndarray) -> np.ndarray:
+    softmax = np.log(softmax_fn(y_pred, 1))
+    log_likelihood = nll_loss(softmax, y_true)
+    return log_likelihood
+
+# ----------------------------- Modules -----------------------------
+# -------------------------------------------------------------------
 class Loss(ABC):
     
     def __init__(self, reduction='mean') -> None:
         self.reduction = reduction
         
     def __call__(self, y_pred:Tensor, y_true:Tensor) -> Any:
-        assert y_pred.matches_shape(y_true), f"Inputs shape don't match y_pred={y_pred.shape}, y_true={y_true.shape}"
+        assert isinstance(y_pred, Tensor) and isinstance(y_true, Tensor), "Inputs must be Tensors"
 
         loss = self.criterion(y_pred, y_true)
         
         # Reduction
         if self.reduction == 'sum':
-            loss = loss.sum()
+            reduction = loss.sum()
         elif self.reduction == 'mean':
-            loss = loss.mean()
+            reduction = loss.mean()
+        else:
+            reduction = loss    
+          
+        if self.reduction is not None:
+            reduction._operation = loss._operation.replace(">", reduction._operation.replace("<", ""))
             
-        return loss
+        return reduction
     
     @abstractmethod
     def criterion(self, y_pred:Tensor, y_true:Tensor) -> Tensor:
@@ -34,47 +72,127 @@ class MSELoss(Loss):
     """ Mean Squared Error Loss: (y_pred - y_true)**2 """
     
     def criterion(self, y_pred:Tensor, y_true:Tensor) -> Tensor:
-        assert isinstance(y_pred, Tensor) and isinstance(y_true, Tensor), "Inputs must be Tensors"
+        assert y_pred.matches_shape(y_true), f"Inputs shape don't match y_pred={y_pred.shape}, y_true={y_true.shape}"
         req_grad = y_pred.requires_grad or y_true.requires_grad
-        loss = Tensor((y_pred.data - y_true.data)**2, (y_pred, y_true), '<MSELoss>', requires_grad=req_grad)
+        mse = mse_loss(y_pred.data, y_true.data)
+        loss = Tensor(mse, (y_pred, y_true), '<MSELoss>', requires_grad=req_grad)
         
         def _backward():
-            grad = 2*(y_pred.data - y_true.data) * loss._grad
-            if y_pred.requires_grad: y_pred._grad += grad # * loss._grad
-            if y_true.requires_grad: y_true._grad += grad # * loss._grad
+            if y_pred.requires_grad: 
+                grad = 2*(y_pred.data - y_true.data) 
+                y_pred._grad += grad * loss._grad
+        
+        loss._backward = _backward
+        
+        return loss
+
+
+class NLLLoss(Loss):
+    """ This class expects y_true to be the probabilities of a LogSoftmax function. Also, y_pred 
+        should be shape=(batch, num_classes). It expects to receive the probability of a sample to be of each class.
+        For binary classification problems, output should not be a scalar value (0 <= x <=1) but an array with 
+        the probability of each class [0.3, 0.7]
+        reference: https://towardsdatascience.com/cross-entropy-negative-log-likelihood-and-all-that-jazz-47a95bd2e81    
+    """
+    
+    def criterion(self, y_pred: Tensor, y_true: Tensor) -> Tensor:
+        assert isinstance(y_pred, Tensor) and isinstance(y_true, Tensor), "Inputs must be Tensors"
+        req_grad = y_pred.requires_grad or y_true.requires_grad
+        log_likelihood = nll_loss(y_pred.data, y_true.data)
+        loss = Tensor(log_likelihood, (y_pred, y_true), '<NLLLoss>', requires_grad=req_grad)
+        
+        def _backward():
+            # Hand made derivation
+            if y_pred.requires_grad:
+                grad = np.zeros(y_pred.shape)
+                for i, true in enumerate(y_true.data):
+                    grad[i][true] = -1.0
+                y_pred._grad += (grad * loss._grad)
+        
+        loss._backward = _backward
+        
+        return loss
+
+   
+class BCELoss(Loss):
+    
+    def criterion(self, y_pred: Tensor, y_true: Tensor) -> Tensor:
+        req_grad = y_pred.requires_grad or y_true.requires_grad
+        bce = bce_loss(y_pred.data, y_true.data)
+        loss = Tensor(bce, (y_pred, y_true), '<BCELoss>', requires_grad=req_grad)
+        
+        def _backward():
+            # Hand made derivation
+            if y_pred.requires_grad:
+                term_0 = -(1 - y_true.data) / ((1 - y_pred.data) + epsilon)
+                term_1 = y_true.data / (y_pred.data + epsilon)
+                y_pred._grad += -(term_0 + term_1) * loss._grad
+        
+        loss._backward = _backward
+        
+        return loss
+
+
+class BCEWithLogitsLoss(Loss):
+    """ This loss combines a Sigmoid layer and the BCELoss in one single class.
+    This version is more numerically stable than using a plain Sigmoid followed by a BCELoss as,
+    by combining the operations into one layer, we take advantage of the log-sum-exp
+    trick for numerical stability
+    
+    References:
+        https://pytorch.org/docs/stable/generated/torch.nn.BCEWithLogitsLoss.html
+        https://stackoverflow.com/questions/66906884/how-is-pytorchs-class-bcewithlogitsloss-exactly-implemented
+    """
+    
+    def criterion(self, y_pred: Tensor, y_true: Tensor) -> Tensor:
+        req_grad = y_pred.requires_grad or y_true.requires_grad
+        bce = bce_with_logits_loss(y_pred.data, y_true.data)
+        loss = Tensor(bce, (y_pred, y_true), '<BCEWithLogitsLoss>', requires_grad=req_grad)
+        
+        def _backward():
+            # Hand made derivation
+            if y_pred.requires_grad:
+                tn = -relu_fn(y_pred.data)
+                dtn = np.where(tn == 0, 0, -1)
+                div1 = -dtn*np.exp(-tn) + (-1-dtn)*np.exp((-y_pred.data-tn))
+                div2 = np.exp(-tn) + np.exp((-y_pred.data-tn))
+                grad = (1 - y_true.data) + dtn + (div1/(div2 + epsilon))
+                
+                y_pred._grad += grad * loss._grad
         
         loss._backward = _backward
         
         return loss
     
-
-# class BCELoss(Loss):
-#     # TODO: Not working properly
-    
-#     def __init__(self) -> None:
-#         pass
-    
-#     def __call__(self, y_pred:np.ndarray, y_true:np.ndarray) -> float:
-#         super().__call__(y_pred, y_true)
-#         y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
-#         term_0 = (1-y_true) * np.log(1-y_pred + epsilon)
-#         term_1 = y_true * np.log(y_pred + epsilon)
-        
-#         return -np.mean(term_0+term_1, axis=0)
-    
     
 class CrossEntropyLoss(Loss):
-    """ Reference: https://deepnotes.io/softmax-crossentropy#:~:text=Cross%20entropy%20indicates%20the%20distance,used%20alternative%20of%20squared%20error"""
-    # TODO: Not working properly
+    """ Same as:\n
+    log_softmax = LogSoftmax(dim=1)(y_pred)\n
+    loss = NLLLoss(reduction=None)(log_softmax, y_true)
+    
+    Reference: https://deepnotes.io/softmax-crossentropy#:~:text=Cross%20entropy%20indicates%20the%20distance,used%20alternative%20of%20squared%20error
+    """
     
     def criterion(self, y_pred: Tensor, y_true: Tensor) -> Tensor:
         """
-        X is the output from fully connected layer (num_examples x num_classes)
-        y is labels (num_examples x 1)
+        y_pred is the output (logits) from fully connected layer (num_examples x num_classes)
+        y_true is labels (num_examples x 1)
             Note that y is not one-hot encoded vector. 
             It can be computed as y.argmax(axis=1) from one-hot encoded vectors of labels if required.
-        """
-        assert isinstance(y_pred, Tensor) and isinstance(y_true, Tensor), "Inputs must be Tensors"
-        return -y_pred[range(y_true.shape[0]), y_true.data.argmax(axis=1)].log().mean()
-                    
+        """        
+        req_grad = y_pred.requires_grad or y_true.requires_grad
+        cross_entropy = cross_entropy_loss(y_pred.data, y_true.data)
+        loss = Tensor(cross_entropy, (y_pred, y_true), '<CrossEntropyLoss>', requires_grad=req_grad)
+        
+        def _backward():
+            # Hand made derivation
+            if y_pred.requires_grad:
+                dlogits = softmax_fn(y_pred.data, 1)
+                n = y_pred.shape[0]
+                dlogits[range(n), y_true.data] -= 1
+                y_pred._grad += (dlogits * loss._grad)
+        
+        loss._backward = _backward
+        
+        return loss
         

synapgrad/nn/neurons.py
@@ -11,7 +11,7 @@ class Neuron(nn.Module):
     def __init__(self, inputs:int, weight_init_method='he') -> None:
         self.inputs = inputs
         # Randomly initialize weights and bias
-        weight_values = np.expand_dims(init_weights(inputs, 1, weight_init_method), 0)
+        weight_values = np.expand_dims(init_weights(inputs, 1, weight_init_method), 0).astype(np.float32)
         self.weights = Tensor(weight_values, requires_grad=True)
         self.bias = Tensor([0], requires_grad=True)
     

tests/test_activations.py
@@ -3,62 +3,41 @@ from synapgrad import Tensor, nn
 from utils import check_tensors
 
 
-def test_relu():
+def check_activation(act, act_t):
     l1 = [[-1.0,-2.0, 4.0, 5.0, 1.0, 7.0], [-1.0,-2.0, 4.0, 5.0, 1.0, 7.0]]
     
     # synapgrad
     a = Tensor(l1, requires_grad=True)
-    out = nn.ReLU()(a)
+    b = (a*4)/7 - 2
+    out = act(b)*b
     out = out.sum()
     out.backward()
     
     # torch
     a_t = torch.tensor(l1, requires_grad=True)
-    out_t = torch.nn.ReLU()(a_t)
+    b_t = (a_t*4)/7 - 2
+    out_t = act_t(b_t)*b_t
     out_t = out_t.sum()
     out_t.backward()
     
+    print(a.grad)
+    print(a_t.grad)
+    
     assert check_tensors(out, out_t)
     assert check_tensors(a.grad, a_t.grad)
+
+
+def test_relu():
+    check_activation(nn.ReLU(), torch.nn.ReLU())
     
     
 def test_sigmoid():
-    l1 = [[-1.0,-2.0, 4.0, 5.0, 1.0, 7.0]]
-    
-    # synapgrad
-    a = Tensor(l1, requires_grad=True)
-    b = (a*4)/7 - 2
-    out = nn.Sigmoid()(b)*b
-    out = out.sum()
-    out.backward()
-    
-    # torch
-    a_t = torch.tensor(l1, requires_grad=True)
-    b_t = (a_t*4)/7 - 2
-    out_t = torch.nn.Sigmoid()(b_t)*b_t
-    out_t = out_t.sum()
-    out_t.backward()
-    
-    assert check_tensors(out, out_t)
-    assert check_tensors(a.grad, a_t.grad)
+    check_activation(nn.Sigmoid(), torch.nn.Sigmoid())
     
     
 def test_softmax():
-    l1 = [[-1.0,-2.0, 4.0, 5.0, 1.0, 7.0]]
+    check_activation(nn.Softmax(dim=1), torch.nn.Softmax(dim=1))
     
-    # synapgrad
-    a = Tensor(l1, requires_grad=True)
-    b = (a*4)/7 - 2
-    out = nn.Softmax(dim=1)(a)*b
-    out = out.sum()
-    out.backward()
     
-    # torch
-    a_t = torch.tensor(l1, requires_grad=True)
-    b_t = (a_t*4)/7 - 2
-    out_t = torch.nn.Softmax(dim=1)(a_t)*b_t
-    out_t = out_t.sum()
-    out_t.backward()
-    
-    assert check_tensors(out, out_t)
-    assert check_tensors(a.grad, a_t.grad)
+def test_log_softmax():
+    check_activation(nn.LogSoftmax(dim=1), torch.nn.LogSoftmax(dim=1))

tests/test_losses.py
@@ -1,6 +1,7 @@
 import torch
 from synapgrad import Tensor, nn
 from utils import check_tensors
+import numpy as np
 
 
 def test_MSELoss():
@@ -31,37 +32,97 @@ def test_MSELoss():
     
     assert check_tensors(loss, loss_t)
     assert check_tensors(inp.grad, inp_t.grad)
+
+
+def test_NLLLoss():
+    pred = [[0.14, 0.3],[-0.2, 0.9],[-3, 0.1]]
+    label = [0, 1, 0]
+    
+    # synapgrad
+    ypred = nn.LogSoftmax(dim=1)(Tensor(pred, requires_grad=True))
+    ypred.retain_grad()
+    ylabel = Tensor(label, dtype=np.int8)
+    loss = nn.NLLLoss()(ypred, ylabel)
+    loss.backward()
+
+    # torch
+    ypred_t = torch.nn.LogSoftmax(dim=1)(torch.tensor(pred, requires_grad=True))
+    ypred_t.retain_grad()
+    ylabel_t = torch.tensor(label).type(torch.LongTensor)
+    loss_t = torch.nn.NLLLoss()(ypred_t, ylabel_t)
+    loss_t.backward()
+
+    print(ypred_t.grad)
+    print(ypred.grad)
+    
+    assert check_tensors(loss, loss_t)
+    assert check_tensors(ypred.grad, ypred_t.grad)
+
+    
+def test_BCELoss():
+    pred = [0.3, 0.4, 0.7]
+    label = [0, 1, 0]
+    
+    # synapgrad
+    ypred = Tensor(pred, requires_grad=True)
+    ylabel = Tensor(label)
+    loss = nn.BCELoss()(ypred, ylabel)
+    loss.backward()
+
+    # torch
+    ypred_t = torch.tensor(pred, requires_grad=True)
+    ylabel_t = torch.tensor(label, dtype=torch.float)
+    loss_t = torch.nn.BCELoss()(ypred_t, ylabel_t)
+    loss_t.backward()
+
+    print(ypred_t.grad)
+    print(ypred.grad)
+    
+    assert check_tensors(loss, loss_t)
+    assert check_tensors(ypred.grad, ypred_t.grad)
+
+
+def test_BCEWithLogitsLoss():
+    pred = [0.3, 0.4, 0.7]
+    label = [0, 1, 0]
+    
+    # synapgrad
+    ypred = Tensor(pred, requires_grad=True)
+    ylabel = Tensor(label)
+    loss = nn.BCEWithLogitsLoss()(ypred, ylabel)
+    loss.backward()
+
+    # torch
+    ypred_t = torch.tensor(pred, requires_grad=True)
+    ylabel_t = torch.tensor(label, dtype=torch.float)
+    loss_t = torch.nn.BCEWithLogitsLoss()(ypred_t, ylabel_t)
+    loss_t.backward()
+
+    print(ypred_t.grad)
+    print(ypred.grad)
+    
+    assert check_tensors(loss, loss_t)
+    assert check_tensors(ypred.grad, ypred_t.grad)
     
     
-# def test_BCELoss():
-#     ...
+def test_CrossEntropyLoss():
+    pred = [[0.14, 0.3],[-0.2, 0.9],[-3, 0.1]]
+    label = [0, 1, 0]
     
+    # synapgrad
+    ypred = Tensor(pred, requires_grad=True)
+    ylabel = Tensor(label, dtype=np.int8)
+    loss = nn.CrossEntropyLoss()(ypred, ylabel)
+    loss.backward()
+
+    # torch
+    ypred_t = torch.tensor(pred, requires_grad=True)
+    ylabel_t = torch.tensor(label).type(torch.LongTensor)
+    loss_t = torch.nn.CrossEntropyLoss()(ypred_t, ylabel_t)
+    loss_t.backward()
+
+    print(ypred_t.grad)
+    print(ypred.grad)
     
-# def test_CrossEntropyLoss():
-#     true = [[1.0, 0.0], [0.0,1.0]]
-#     pred = [[[1, 0.0], [0.0, 1.0]]]
-    
-#     # synapgrad
-#     y_true = Tensor(true)
-#     inp = Tensor(pred, requires_grad=True)
-    
-#     y_pred = nn.Softmax(dim=1)(inp)
-#     y_pred = y_pred.squeeze()
-#     loss = nn.CrossEntropyLoss(reduction='mean')(y_pred, y_true)
-#     loss.backward()
-    
-#     print(loss)
-    
-#     # torch
-#     y_true_t = torch.tensor(true)
-#     inp_t = torch.tensor(pred, requires_grad=True)
-    
-#     y_pred_t = torch.nn.Softmax(dim=1)(inp_t)
-#     y_pred_t = y_pred_t.squeeze()
-#     loss_t = torch.nn.CrossEntropyLoss(reduction='mean')(y_pred_t, y_true_t)
-#     loss_t.backward()
-    
-#     print(loss_t)
-    
-#     assert check_tensors(loss, loss_t)
-#     assert check_tensors(inp.grad, inp_t.grad)
+    assert check_tensors(loss, loss_t)
+    assert check_tensors(ypred.grad, ypred_t.grad)

tests/utils.py
@@ -3,7 +3,8 @@ import numpy as np
 from synapgrad import Tensor
 
 
-tolerance = 1e-5
+rtol = 0.00001
+atol = 1e-8
 
 def check_tensors(t1:Tensor, t2:torch.Tensor) -> bool:
     """Returns if 2 tensors have the same values and shape
@@ -14,7 +15,9 @@ def check_tensors(t1:Tensor, t2:torch.Tensor) -> bool:
     """
     if t1 is None or t2 is None: 
         return t1 is t2
-    shape_cond = t1.matches_shape(t2)
-    values_cond = abs(t1.detach().sum().item() - t2.detach().sum().item()) < abs(tolerance*(np.max(t1.data)+1))
     
-    return shape_cond and values_cond
+    t1_t = torch.from_numpy(t1.data)
+    
+    assert t1_t.dtype == t2.dtype, f"dtype of tensors don't match t1={t1.dtype} t2={t2.dtype}"
+    
+    return torch.equal(t1_t, t2) or torch.allclose(t1_t, t2, rtol=rtol, atol=atol)