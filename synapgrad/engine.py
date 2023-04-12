fixing Softmax error in backprop

synapgrad/engine.py
@@ -6,6 +6,8 @@ from typing import Iterable, Union
 gradient__ = True
 retain_grads__ = False
 
+default_type__ = np.float32
+
 class no_grad:
     
     def __init__(self) -> None:
@@ -38,15 +40,15 @@ def manual_seed(seed:int):
     random.seed(seed)
 
 
-def tensor(data, requires_grad=False, dtype=np.float32) -> 'Tensor':
+def tensor(data, requires_grad=False, dtype=None) -> 'Tensor':
     return Tensor(data, requires_grad=requires_grad, dtype=dtype)
 
 
 class Tensor:
     
-    def __init__(self, data, _children=(), _operation=None, requires_grad=False, dtype=np.float32) -> None:
+    def __init__(self, data, _children=(), _operation=None, requires_grad=False, dtype=None) -> None:
         if not isinstance(data, np.ndarray):
-            data = np.array(data, dtype=dtype)
+            data = np.array(data, dtype=default_type__)
         if dtype is not None and data.dtype != dtype: data = data.astype(dtype)
         assert isinstance(data, np.ndarray), "data must be a list or numpy array"
         

synapgrad/nn/activations.py
@@ -57,7 +57,6 @@ class Sigmoid(nn.Module):
 
     
 class Softmax(nn.Module):
-    # TODO: Not working properly
     """ References: 
             https://eli.thegreenplace.net/2016/the-softmax-function-and-its-derivative/
             https://aimatters.wordpress.com/2019/06/17/the-softmax-function-derivative/
@@ -75,11 +74,9 @@ class Softmax(nn.Module):
         def _backward():
             # Hand made derivation
             if x.requires_grad:
-                diag = np.stack([np.diag(v) for v in softmax])
-                S_matrix = np.repeat(softmax, softmax.shape[self.dim], axis=self.dim).reshape(diag.shape)
-                S_matrix_T = S_matrix.swapaxes(-2, -1)
-                grad = diag - (S_matrix*S_matrix_T)
-                x._grad += grad.sum(axis=2) * out._grad
+                jacobians = np.stack([np.diag(y) - np.outer(y, y) for y in softmax])
+                out_grad = np.expand_dims(out._grad, axis=self.dim)
+                x._grad += (out_grad @ jacobians).sum(axis=self.dim)
             
         out._backward = _backward
         
@@ -87,7 +84,7 @@ class Softmax(nn.Module):
 
     
 class LogSoftmax(nn.Module):
-    # TODO: Not working properly
+    # TODO: Not as fast as posible
     
     def __init__(self, dim) -> None:
         super().__init__()
@@ -95,18 +92,19 @@ class LogSoftmax(nn.Module):
     
     def forward(self, x: Tensor) -> Tensor:
         assert isinstance(x, Tensor), "Input must be a Tensor"
+        return Softmax(dim=self.dim)(x).log()
+        
+        # Implement hand made derivation correctly
         softmax = softmax_fn(x.data, self.dim)
         log_softmax = np.log(softmax)
         out = Tensor(log_softmax, (x,), '<LogSoftmax>', requires_grad=x.requires_grad)
     
         def _backward():
-            # Hand made derivation
+            
             if x.requires_grad:
-                I = np.stack((np.identity(softmax.shape[self.dim]),)*softmax.shape[0])
-                S_matrix = np.stack([np.tile(v, softmax.shape[self.dim]) for v in softmax]).reshape(I.shape)
-                S_matrix_T = np.stack([np.transpose(m) for m in S_matrix])
-                grad = I - S_matrix_T
-                x._grad += grad.sum(axis=self.dim) * out._grad
+                jacobians = np.stack([np.identity(len(y)) - np.outer(y, y) for y in softmax])
+                out_grad = np.expand_dims(out._grad, axis=self.dim)
+                x._grad += (out_grad @ jacobians).sum(axis=self.dim)
             
         out._backward = _backward
         

tests/test_activations.py
@@ -4,7 +4,7 @@ from utils import check_tensors
 
 
 def check_activation(act, act_t):
-    l1 = [[-1.0,-2.0, 4.0, 5.0, 1.0, 7.0], [-1.0,-2.0, 4.0, 5.0, 1.0, 7.0]]
+    l1 = [[-1.0,-2.0, 4.0, 5.0, 1.0, 7.0], [-1.0,-2.0, 2.0, 1.0, 10.0, -4.0]]
     
     # synapgrad
     a = Tensor(l1, requires_grad=True)
@@ -35,9 +35,9 @@ def test_sigmoid():
     check_activation(nn.Sigmoid(), torch.nn.Sigmoid())
     
     
-# def test_softmax():
-#     check_activation(nn.Softmax(dim=1), torch.nn.Softmax(dim=1))
+def test_softmax():
+    check_activation(nn.Softmax(dim=1), torch.nn.Softmax(dim=1))
     
     
-# def test_log_softmax():
-#     check_activation(nn.LogSoftmax(dim=1), torch.nn.LogSoftmax(dim=1))
+def test_log_softmax():
+    check_activation(nn.LogSoftmax(dim=1), torch.nn.LogSoftmax(dim=1))