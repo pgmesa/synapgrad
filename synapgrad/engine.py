fixing matmul backward calculation and finishing Conv2d layer

synapgrad/engine.py
@@ -54,7 +54,7 @@ class Tensor:
             data (number or iterable): data of the tensor, must be convertible into a numpy.array().
             _children (tuple, optional): tensors which produced this tensor as a result of an operation. Defaults to ().
             _operation (str, optional): string that represents the operation that created this tensor. Defaults to None.
-            requires_grad (bool, optional): Whether this tensor requieres gradients or not. Defaults to False.
+            requires_grad (bool, optional): whether this tensor requieres gradients or not. Defaults to False.
             dtype (type, optional): numpy type of this tensor data. Defaults to None.
             
         """
@@ -92,7 +92,6 @@ class Tensor:
         def get_incompatible_dims(tensor_shape, grad_shape) -> tuple:
             not_compatible_dims = []
             for i, (tdim, gdim) in enumerate(zip(tensor_shape[::-1], grad_shape[::-1])):
-                print(tdim, gdim)
                 if gdim != 1 and tdim != gdim:
                     not_compatible_dims.append(len(grad_shape)-1-i)
             not_compatible_dims = tuple(not_compatible_dims)
@@ -160,13 +159,13 @@ class Tensor:
         
         def _backward():
             if self.requires_grad:
-                self._grad += np.dot(tensor.data, out._grad.T).T
+                grad = out._grad @ np.moveaxis(tensor.data, -2, -1)
+                self.__add_grad(self, grad)
             
             if tensor.requires_grad:
-                print(self.data.shape, np.moveaxis(self.data, -1, -2).shape, out._grad.shape, tensor._grad.shape)
-                print(np.dot(np.moveaxis(self.data, -1, -2), out._grad).shape)
-                grad = np.dot(np.moveaxis(self.data, -1, -2), out._grad)
-                tensor._grad += grad # grad.sum(axis=(0,2))
+                grad = np.moveaxis(out._grad, -2,-1) @ self.data
+                grad = np.moveaxis(grad, -2,-1)
+                self.__add_grad(tensor, grad)
                     
         out._backward = _backward
         
@@ -282,25 +281,13 @@ class Tensor:
         return out
     
     
-    @staticmethod
-    def add(t1:'Tensor', t2:'Tensor') -> 'Tensor':
-        return t1 + t2
-    
     def add(self, t2:'Tensor') -> 'Tensor':
         return self + t2
     
     
-    @staticmethod
-    def mul(t1:'Tensor', t2:'Tensor') -> 'Tensor':
-        return t1 * t2
-    
     def mul(self, t2:'Tensor') -> 'Tensor':
         return self * t2
     
-    
-    @staticmethod
-    def matmul(t1:'Tensor', t2:'Tensor') -> 'Tensor':
-        return t1 @ t2
     
     def matmul(self, t2:'Tensor') -> 'Tensor':
         return self @ t2
@@ -465,29 +452,6 @@ class Tensor:
         return out
     
     
-    def sum(self, dim:int=None, keepdims=False) -> 'Tensor':
-        out = Tensor(self.data.sum(axis=dim, keepdims=keepdims), (self,), '<Sum>', requires_grad=self.requires_grad)
-        
-        def _backward():
-            if self.requires_grad:
-                self._grad += out._grad
-            
-        out._backward = _backward
-        
-        return out
-    
-    
-    def mean(self, dim:int=None, keepdims=False) -> 'Tensor':
-        out = Tensor(self.data.mean(axis=dim, keepdims=keepdims), (self,), '<Mean>', requires_grad=self.requires_grad)
-        
-        def _backward():
-            if self.requires_grad:
-                self._grad += (np.ones(self.shape) / self.data.size) * out._grad
-            
-        out._backward = _backward
-        
-        return out
-    
     @staticmethod
     def __get_selected_from_indices(values:np.ndarray, indices:np.ndarray, dim) -> np.ndarray:
         """
@@ -521,17 +485,51 @@ class Tensor:
     
         selected = np.zeros_like(values)
         
-        slices = get_indices(values, dim)
-        slices = list(slices)
-        slices.append(indices)
-        slices = tuple(slices)
+        if dim is not None:
+            slices = get_indices(values, dim)
+            slices = list(slices)
+            slices.append(indices)
+            slices = tuple(slices)
+        else:
+            slices = np.unravel_index(indices, values.shape)
 
         selected[slices] = 1
         
         return selected
     
     
-    def max(self, dim:int=None, keepdims=False, return_selected=False) -> tuple['Tensor',...]:
+    def sum(self, dim:int=None, keepdims=False) -> 'Tensor':
+        out = Tensor(self.data.sum(axis=dim, keepdims=keepdims), (self,), '<Sum>', requires_grad=self.requires_grad)
+        
+        def _backward():
+            if self.requires_grad:
+                self._grad += out._grad
+            
+        out._backward = _backward
+        
+        return out
+    
+    
+    def mean(self, dim:int=None, keepdims=False) -> 'Tensor':
+        out = Tensor(self.data.mean(axis=dim, keepdims=keepdims), (self,), '<Mean>', requires_grad=self.requires_grad)
+        
+        def _backward():
+            if self.requires_grad:
+                if dim is None:
+                    size = self.data.size
+                elif isinstance(dim, int):
+                    size = self.data.shape[dim]
+                elif isinstance(dim, tuple):
+                    size = 1
+                    for d in dim: size *= self.data.shape[d]
+                self._grad += (np.ones(self.shape) / size) * out._grad
+            
+        out._backward = _backward
+        
+        return out
+
+    
+    def max(self, dim:int=None, keepdims=False, return_indices=None, return_selected=False) -> tuple['Tensor',...]:
         max_values = self.data.max(axis=dim, keepdims=keepdims)
         max_indices = self.data.argmax(axis=dim)
         selected = self.__get_selected_from_indices(self.data, max_indices, dim)
@@ -541,20 +539,24 @@ class Tensor:
         def _backward():
             if self.requires_grad:
                 grad = out._grad
-                if not keepdims:
+                if not keepdims and dim is not None:
                     grad = np.expand_dims(grad, axis=dim)
                 self._grad += selected * grad
             
         out._backward = _backward
         
-        out_tuple = (out, max_indices)
+        out_tuple = (out,)
+        
+        if return_indices is not False and dim is not None:
+            out_tuple += (max_indices,)
+        
         if return_selected:
             out_tuple += (selected,)
         
-        return out_tuple
+        return out_tuple if len(out_tuple) > 1 else out_tuple[0]
     
     
-    def min(self, dim:int=None, keepdims=False, return_selected=False) -> tuple['Tensor',...]:
+    def min(self, dim:int=None, keepdims=False, return_indices=None, return_selected=False) -> tuple['Tensor',...]:
         min_values = self.data.min(axis=dim, keepdims=keepdims)
         min_indices = self.data.argmin(axis=dim)
         selected = self.__get_selected_from_indices(self.data, min_indices, dim)
@@ -564,17 +566,21 @@ class Tensor:
         def _backward():
             if self.requires_grad:
                 grad = out._grad
-                if not keepdims:
+                if not keepdims and dim is not None:
                     grad = np.expand_dims(grad, axis=dim)
                 self._grad += selected * grad
             
         out._backward = _backward
         
-        out_tuple = (out, min_indices)
+        out_tuple = (out,)
+        
+        if return_indices is not False and dim is not None:
+            out_tuple += (min_indices,)
+        
         if return_selected:
             out_tuple += (selected,)
         
-        return out_tuple
+        return out_tuple if len(out_tuple) > 1 else out_tuple[0]
     
     
     def transpose(self, dim0:int, dim1:int) -> 'Tensor':

synapgrad/nn/layers.py
@@ -203,6 +203,13 @@ class Conv2d(nn.Module):
             stride = kernel_size
         else:
             stride = np.broadcast_to(stride, 2)
+        
+        if padding == 'same':
+            if any(s != 1 for s in stride):
+                raise ValueError("padding='same' is not supported for strided convolutions")
+            padding = int(np.floor(kernel_size[0] / 2))
+        if padding == 'valid':
+            padding = 0
         padding = np.broadcast_to(padding, 2)
         dilation = np.broadcast_to(dilation, 2)
         
@@ -228,7 +235,10 @@ class Conv2d(nn.Module):
                            padding=self.padding, pad_value=0)(x)
         
         weights = self.weights.view(self.weights.shape[0], -1).transpose(0,1) 
-        convolved = (unfolded.transpose(1,2) @ weights) + self.bias
+        print("Matmul shapes", unfolded.transpose(1,2).shape, weights.shape)
+        mult = unfolded.transpose(1,2) @ weights
+        print("Result", mult.shape)
+        convolved = (mult) + self.bias
         out = convolved.transpose(1,2).view(N, self.out_channels, lH, lW)
 
         return out
@@ -244,11 +254,13 @@ class Conv2d(nn.Module):
     
 class BatchNorm2d(nn.Module):
     """
+    TODO: Gradient not working properly
     Reference:
         https://pytorch.org/docs/stable/generated/torch.nn.BatchNorm2d.html
     """
     
-    def __init__(self, num_features:int, eps:float=1e-5, momentum:float=0.1, affine:bool=True) -> None:
+    def __init__(self, num_features:int, eps:float=1e-5, momentum:float=0.1, affine:bool=True,
+                 track_running_stats:bool=True) -> None:
         """
         Computes the batchnorm2d of a 4-dimensional tensor and its gradient.
 
@@ -258,75 +270,139 @@ class BatchNorm2d(nn.Module):
         beta: tensor of shape (C,) representing the normalization bias.
         eps: small constant to avoid division by zero in variance computation.
         momentum: exponential moving average factor for the mean and variance.
-        
+        track_running_stats: 
         """
         super().__init__()
         self.num_features = num_features
         self.eps = eps
         self.momentum = momentum
         self.affine = affine
+        self.track_running_stats = track_running_stats
         
-        self.gamma = Tensor(np.ones(num_features), requires_grad=self.affine, dtype=np.float32)
-        self.beta = Tensor(np.zeros(num_features), requires_grad=self.affine, dtype=np.float32)
+        self.running_mean = None
+        self.running_var = None
         
+        if affine:
+            self.gamma = Tensor(np.ones(num_features), requires_grad=True, dtype=np.float32)
+            self.beta = Tensor(np.zeros(num_features), requires_grad=True, dtype=np.float32)
+            
+            
     def forward(self, x: Tensor) -> Tensor:
         N, C, H, W = x.shape
+        num_examples = N*H*W
         assert C == self.num_features, f"Expected {self.num_features} channels, got {C}."
         # Compute the mean of each channel
-        mu = np.mean(x.data, axis=(0,2,3), keepdims=True)
+        mu = x.mean(dim=(0,2,3), keepdims=True)
 
         # Compute the variance of each channel
-        var = np.mean((x.data - mu)**2, axis=(0,2,3), keepdims=True)
+        var = ((x - mu)**2).mean(dim=(0,2,3), keepdims=True)
+        std = (var + self.eps).sqrt()
 
         # Update the running average of mean and variance
-        if hasattr(self, 'running_mean'):
-            self.running_mean = self.momentum * self.running_mean + (1 - self.momentum) * mu
-        else:
-            self.running_mean = mu
-        if hasattr(self, 'running_var'):
-            self.running_var = self.momentum * self.running_var + (1 - self.momentum) * var
-        else:
-            self.running_var = var
+        if self.track_running_stats:
+            if self.running_mean is None:
+                self.running_mean = mu.detach().squeeze()
+            else:
+                self.running_mean = (self.momentum * self.running_mean + (1 - self.momentum) * mu.detach()).squeeze()
+            
+            if self.running_var is None:
+                self.running_var = var.detach().squeeze()
+            else:
+                self.running_var = (self.momentum * self.running_var + (1 - self.momentum) * var.detach()).squeeze()
 
         # Normalize the input tensor
-        x_norm = (x.data - mu) / np.sqrt(var + self.eps)
+        x_norm = (x - mu) / std
 
         # Scale and shift the normalization
         if self.affine:
-            out_data = self.gamma.data.reshape(1,C,1,1) * x_norm + self.beta.data.reshape(1,C,1,1)
+            out = self.gamma.view(1,C,1,1) * x_norm + self.beta.view(1,C,1,1)
         else:
             out = x_norm
         
-        out = Tensor(out_data, (x, self.gamma, self.beta), "<BatchNorm2d>", requires_grad=x.requires_grad)
-        
-        def _backward():
-            grad = out._grad
-            
-            # Update gamma and beta gradient
-            if self.gamma.requires_grad:
-                dgamma = np.sum(x_norm * grad, axis=(0,2,3))
-                self.gamma._grad += dgamma
-            if self.beta.requires_grad:
-                dbeta = np.sum(grad, axis=(0,2,3))
-                self.beta._grad += dbeta
-
-            if x.requires_grad:
-                dx_norm = grad * self.gamma.data.reshape(1,C,1,1)
-                if self.affine:
-                    dvar = np.sum(dx_norm * (x.data - mu) * -0.5 * (var + self.eps)**(-3/2), axis=(0,2,3), keepdims=True)
-                    dmu = np.sum(dx_norm * -1 / np.sqrt(var + self.eps), axis=(0,2,3), keepdims=True)
-                    dx = dx_norm / np.sqrt(var + self.eps) + dvar * 2 * (x.data - mu) / (N*H*W) + dmu / (N*H*W)
-                else:
-                    dx = dx_norm / np.sqrt(var + self.eps)
-
-                x._grad += dx
-                
-        out._backward = _backward
-        
         return out
+        
+    # def forward(self, x: Tensor) -> Tensor:
+    #     N, C, H, W = x.shape
+    #     num_examples = N*H*W
+    #     assert C == self.num_features, f"Expected {self.num_features} channels, got {C}."
+    #     # Compute the mean of each channel
+    #     mu = np.mean(x.data, axis=(0,2,3), keepdims=True)
+
+    #     # Compute the variance of each channel
+    #     var = np.mean((x.data - mu)**2, axis=(0,2,3), keepdims=True)
+    #     std = np.sqrt(var + self.eps)
+
+    #     # Update the running average of mean and variance
+    #     if self.track_running_stats:
+    #         if self.running_mean is None:
+    #             self.running_mean = mu
+    #         else:
+    #             self.running_mean = self.momentum * self.running_mean + (1 - self.momentum) * mu
+            
+    #         if self.running_var is None:
+    #             self.running_var = var
+    #         else:
+    #             self.running_var = self.momentum * self.running_var + (1 - self.momentum) * var
+
+    #     # Normalize the input tensor
+    #     x_minus_mean = (x.data - mu) 
+    #     x_norm = x_minus_mean / np.sqrt(var + self.eps)
+
+    #     # Scale and shift the normalization
+    #     if self.affine:
+    #         out_data = self.gamma.data.reshape(1,C,1,1) * x_norm + self.beta.data.reshape(1,C,1,1)
+    #     else:
+    #         out_data = x_norm
+        
+    #     chidren = (x,) if not self.affine else (x, self.gamma, self.beta)
+    #     out = Tensor(out_data, chidren, "<BatchNorm2d>", requires_grad=x.requires_grad)
+        
+    #     def _backward():
+    #         grad = out._grad
+            
+    #         # Update gamma and beta gradient
+    #         if self.affine:
+    #             dgamma = np.sum(grad * x_norm, axis=(0,2,3))
+    #             self.gamma._grad += dgamma
+    #             dbeta = np.sum(grad, axis=(0,2,3))
+    #             self.beta._grad += dbeta
+
+    #         if x.requires_grad:  
+    #             # standard_grad = grad
+
+    #             # if self.affine: standard_grad *= self.gamma.data.reshape(1,C,1,1)
+
+    #             # var_grad = np.sum(standard_grad * x_minus_mean * -0.5 * var ** (-3/2), axis=(0,2,3), keepdims=True)
+    #             # stddev_inv = 1 / std
+    #             # aux_x_minus_mean = 2 * x_minus_mean / num_examples
+
+    #             # mean_grad = (np.sum(standard_grad * -stddev_inv, axis=(0,2,3), keepdims=True) + var_grad * np.sum(-aux_x_minus_mean, axis=(0,2,3), keepdims=True))
+
+    #             # x._grad += standard_grad * stddev_inv + var_grad * aux_x_minus_mean + mean_grad / num_examples
+                
+    #             dx_norm = grad
+    #             if self.affine: standard_grad *= self.gamma.data.reshape(1,C,1,1)
+    #             dvar = np.sum(dx_norm * (x.data - mu) * -0.5 * (var + self.eps)**(-3/2), axis=(0,2,3), keepdims=True)
+    #             dmu = np.sum(dx_norm * -1 / np.sqrt(var + self.eps), axis=(0,2,3), keepdims=True)
+    #             dx = (dx_norm / np.sqrt(var + self.eps))    +    (dvar * 2 * (x.data - mu) / (N*H*W))   +   (dmu / (N*H*W))
+                
+    #             x._grad += dx
+                 
+    #             # if self.affine:
+    #             #     dx_norm = grad * self.gamma.data.reshape(1,C,1,1) 
+    #             #     dvar = np.sum(dx_norm * (x.data - mu) * -0.5 * (var + self.eps)**(-3/2), axis=(0,2,3), keepdims=True)
+    #             #     dmu = np.sum(dx_norm * -1 / np.sqrt(var + self.eps), axis=(0,2,3), keepdims=True)
+    #             #     dx = (dx_norm / np.sqrt(var + self.eps))    +    (dvar * 2 * (x.data - mu) / (N*H*W))   +   (dmu / (N*H*W))
+    #             # else:
+    #             #     dx = grad / np.sqrt(var + self.eps)
+    #             #     print(np.max(dx))            
+                
+    #     out._backward = _backward
+        
+    #     return out
 
     def parameters(self) -> list[Tensor]:
-        return [self.gamma, self.beta]
+        return [self.gamma, self.beta] if self.affine else []
 
 class Dropout(nn.Module):
     """ 

tests/test_engine.py
@@ -51,17 +51,37 @@ def test_engine_v2():
     # synapgrad
     a = Tensor(l1, requires_grad=True)
     b = Tensor(l2, requires_grad=True)
-    c = (a.exp()+b)*b.log().sqrt()
+    c = (a.exp()+b)*b.log().sqrt().mean(dim=0, keepdims=True)
     c.sum().backward()
     
     # torch
     a_t = torch.tensor(l1, requires_grad=True)
     b_t = torch.tensor(l2, requires_grad=True)
-    c_t = (a_t.exp()+b_t)*b_t.log().sqrt()
+    c_t = (a_t.exp()+b_t)*b_t.log().sqrt().mean(dim=0, keepdims=True)
     c_t.sum().backward()
     
     assert check_tensors(c, c_t)
     assert check_tensors(b.grad, b_t.grad)
+    
+    
+def test_engine_v3():
+    l1 = np.random.randint(0, 10, size=(1,2,4,6)).astype(np.float32)
+    
+    # synapgrad
+    a = Tensor(l1, requires_grad=True)
+    c = a.mean(dim=(0,2,3), keepdims=True).max()
+    c.sum().backward()
+    
+    # torch
+    a_t = torch.tensor(l1, requires_grad=True)
+    c_t = a_t.mean(dim=(0,2,3), keepdims=True).max()
+    c_t.sum().backward()
+    
+    print(a.grad)
+    print(a_t.grad)
+    
+    assert check_tensors(c, c_t)
+    assert check_tensors(a.grad, a_t.grad)
     
 
 def test_engine_unfold():

tests/test_layers.py
@@ -111,12 +111,66 @@ def test_maxpool2d():
     assert check_tensors(inp.grad, inp_t.grad)
     
 
-# def test_conv2d():
-#     ...
+def test_conv2d():
+    l = np.random.rand(2,3,4,4).astype(np.float32)
+    out_channels = 5; kernel_size = 3; stride = 1; padding = 'same'
+
+    inp = Tensor(l, requires_grad=True)
+    conv = nn.Conv2d(l.shape[1], out_channels, kernel_size, stride, padding)
+    out = conv(inp)
+    out.sum().backward()
+    
+    inp_t = torch.tensor(l, requires_grad=True)
+    conv_t = torch.nn.Conv2d(l.shape[1], out_channels, kernel_size, stride, padding)
+    out_t = conv_t(inp_t)
+    out_t.sum().backward()
+
+    print(out_t.shape)
+    print(out.shape)
+    
+    assert out.matches_shape(out_t.detach().numpy())
+
+    params_t = list(conv_t.parameters())
+    params = list(conv.parameters())
+    for i, (p_t, p) in enumerate(zip(params_t, params)):
+        print(f"Param {i+1}")
+        print(p_t); print(p)
+        print(p_t.grad); print(p.grad)
+        check_tensors(p.grad, p_t.grad)
     
     
 # def test_batchnorm2d():
-#     ...
+#     l = np.random.randn(1,2,4,4).astype(np.float32)
+#     momentum = 0.1; affine = False;  track_running_stats = False
+
+#     bnorm_t = torch.nn.BatchNorm2d(l.shape[1], momentum=momentum, affine=affine, track_running_stats=track_running_stats)
+
+#     inp_t = torch.tensor(l, requires_grad=True)
+#     out_t = bnorm_t(inp_t)
+#     out_t.sum().backward()
+
+#     inp = Tensor(l, requires_grad=True)
+#     bnorm = nn.BatchNorm2d(inp.shape[1], momentum=momentum, affine=affine, track_running_stats=track_running_stats)
+#     out = bnorm(inp)
+#     out.sum().backward()
+
+#     print(out_t)
+#     print(out)
+
+#     print(inp_t.grad)
+#     print(inp.grad)
+    
+#     check_tensors(out, out_t)
+#     check_tensors(inp.grad, inp_t.grad)
+
+#     params_t = list(bnorm_t.parameters())
+#     params = list(bnorm.parameters())
+#     for i, (p_t, p) in enumerate(zip(params_t, params)):
+#         print(f"Param {i+1}")
+#         print(p_t); print(p)
+#         print(p_t.grad); print(p.grad)
+#         check_tensors(p, p_t)
+#         check_tensors(p.grad, p_t.grad)
     
 
 def test_dropout():